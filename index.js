20d106d786a49793b638fa184fe6b4ec65362b24
Cecilia Carlson <cecilia.carlson03@gmail.com>
2025-07-27 12:35:41 -0300

    JWT

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ import "dotenv/config";
 import express from "express";
 const app = express();
 
+app.use(express.json());
+
 app.get("/",(rep, res) => {
     res.json("Api Rest en Node.js");
 });
 
 import productsRouter from "./src/routes/products.router.js";
 app.use("/api", productsRouter);
+
+import authRouter from "./src/routes/auth.router.js";
+app.use("/api/auth", authRouter);
 
 const PORT = process.env.PORT || 3001;
 
